86cbeb64ae983daff77a67a2c5a535599e281e37
Elizabeth142 <elizabethreji142@gmail.com>
2026-09-10 13:35:25 +0530

    Update Expense Tracker

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let transactions=JSON.parse(localStorage.getItem("transactions")) || [];
 
+
+/* DOM ELEMENTS*/
+
 const transactionForm=document.getElementById("transaction_form");
 const transactionList=document.getElementById("transaction-list");
 const totalIncome=document.getElementById("total_income");
@@ -9,6 +12,7 @@ const emptyMessage=document.getElementById("empty-message");
 const submitButton=document.getElementById("submit-btn");
 const typeFilter=document.getElementById("type-filter");
 const categoryFilter=document.getElementById("category-filter");
+const sortOrder=document.getElementById("sort-order");
 const errorMessage=document.getElementById("error-message");
 const monthFilter=document.getElementById("month-filter");
 const yearFilter=document.getElementById("year-filter");
@@ -17,11 +21,23 @@ const monthlyExpenses=document.getElementById("monthly-expenses");
 const monthlyBalance=document.getElementById("monthly-balance");
 const deletePopup=document.getElementById("delete-popup");
 const cancelDelete=document.getElementById("cancel-delete");
-const confirmDelete = document.getElementById("confirm-delete");
+const confirmDelete=document.getElementById("confirm-delete");
 const type=document.getElementById("type");
 const category=document.getElementById("category");
+const cancelEdit=document.getElementById("cancel-edit");
+const formTitle=document.getElementById("form-title");
 
 
+/* DATE*/
+
+const dateInput=document.getElementById("date");
+
+const today=new Date().toISOString().split("T")[0];
+
+dateInput.max=today;
+
+
+/* CATEGORIES*/
 
 const incomeCategories=[
     "Salary",
@@ -54,41 +70,52 @@ const categories=[
     "Other"
 ];
 
-categories.forEach(function(category){
-    const option=document.createElement("option");
 
-    option.value=category;
-    option.textContent=category;
+function loadCategoryFilter(selectedType){
 
-    categoryFilter.appendChild(option);
-});
+    categoryFilter.innerHTML='<option value="all">All categories</option>';
+
+    let categoryList=[];
+
+    if(selectedType==="income"){
+
+        categoryList=incomeCategories;
+
+    }else if(selectedType==="expense"){
+
+        categoryList=expenseCategories;
+
+    }else{
+
+        categoryList=categories;
+    }
+
+    categoryList.forEach(function(categoryName){
+
+        const option=document.createElement("option");
+
+        option.value=categoryName;
+        option.textContent=categoryName;
+
+        categoryFilter.appendChild(option);
+    });
+}
+
+
+/* STATE*/
 
 let deleteId=null;
 let editingId=null;
 
+
+/*SAVE TRANSACTIONS*/
 
 function saveTransactions(){
     localStorage.setItem("transactions",JSON.stringify(transactions));
 }
 
 
-typeFilter.addEventListener("change",function(){
-    displayTransactions();
-});
-
-
-categoryFilter.addEventListener("change",function(){
-    displayTransactions();
-});
-
-
-monthFilter.addEventListener("change",function(){
-    updateMonthlySummary();
-});
-
-yearFilter.addEventListener("change",function(){
-    updateMonthlySummary();
-});
+/*TYPE AND CATEGORY*/
 
 type.addEventListener("change",function(){
 
@@ -115,6 +142,36 @@ type.addEventListener("change",function(){
         category.appendChild(option);
     });
 });
+
+
+/*FILTER EVENTS*/
+
+typeFilter.addEventListener("change", function () {
+    loadCategoryFilter(typeFilter.value);
+    displayTransactions();
+});
+
+
+categoryFilter.addEventListener("change",function(){
+    displayTransactions();
+});
+
+
+monthFilter.addEventListener("change",function(){
+    updateMonthlySummary();
+});
+
+sortOrder.addEventListener("change",function(){
+    displayTransactions();
+});
+
+yearFilter.addEventListener("change", function () {
+    loadMonths(yearFilter.value);
+    updateMonthlySummary();
+});
+
+
+/*ADD / UPDATE TRANSACTION*/
 
 transactionForm.addEventListener("submit",function(event){
 
@@ -149,6 +206,12 @@ transactionForm.addEventListener("submit",function(event){
 
     if(date===""){
         errorMessage.textContent="Please select a date.";
+        return;
+    }
+
+
+    if(date>today){
+        errorMessage.textContent="Date cannot be in the future.";
         return;
     }
 
@@ -200,6 +263,8 @@ transactionForm.addEventListener("submit",function(event){
 });
 
 
+/*DISPLAY TRANSACTIONS*/
+
 function displayTransactions(){
 
     transactionList.innerHTML="";
@@ -221,6 +286,18 @@ function displayTransactions(){
         return typeMatches && categoryMatches;
     });
 
+    if(sortOrder.value==="ascending"){
+
+        filteredTransactions.sort(function(a,b){
+            return new Date(a.date)-new Date(b.date);
+        });
+
+    }else if(sortOrder.value==="descending"){
+
+        filteredTransactions.sort(function(a,b){
+            return new Date(b.date)-new Date(a.date);
+        });
+    }
 
     if(filteredTransactions.length===0){
 
@@ -236,84 +313,92 @@ function displayTransactions(){
 
     filteredTransactions.forEach(function(transaction){
 
-    const row=document.createElement("tr");
+        const row=document.createElement("tr");
 
 
-    const formattedDate=new Date(
-        transaction.date+"T00:00:00"
-    ).toLocaleDateString("en-IN");
+        const formattedDate=new Date(
+            transaction.date+"T00:00:00"
+        ).toLocaleDateString("en-IN");
 
 
-    const formattedAmount=transaction.amount.toLocaleString("en-IN",{
-        minimumFractionDigits:2,
-        maximumFractionDigits:2
+        const formattedAmount=transaction.amount.toLocaleString("en-IN",{
+            minimumFractionDigits:2,
+            maximumFractionDigits:2
+        });
+
+
+        const amountDisplay=
+            transaction.type==="income"
+                ? `+ ₹${formattedAmount}`
+                : `- ₹${formattedAmount}`;
+
+
+        const dateCell=document.createElement("td");
+        dateCell.textContent=formattedDate;
+
+
+        const typeCell=document.createElement("td");
+
+        const typeSpan=document.createElement("span");
+        typeSpan.className=`transaction-type ${transaction.type}`;
+        typeSpan.textContent=transaction.type;
+
+        typeCell.appendChild(typeSpan);
+
+
+        const categoryCell=document.createElement("td");
+        categoryCell.textContent=transaction.category;
+
+
+        const descriptionCell=document.createElement("td");
+        descriptionCell.textContent=transaction.description || "-";
+
+
+        const amountCell=document.createElement("td");
+
+        amountCell.className=
+            transaction.type==="income"
+                ? "amount-income"
+                : "amount-expense";
+
+        amountCell.textContent=amountDisplay;
+
+
+        const actionsCell=document.createElement("td");
+
+
+        const editButton=document.createElement("button");
+        editButton.textContent="Edit";
+
+        editButton.addEventListener("click",function(){
+            editTransaction(transaction.id);
+        });
+
+
+        const deleteButton=document.createElement("button");
+        deleteButton.textContent="Delete";
+
+        deleteButton.addEventListener("click",function(){
+            deleteTransaction(transaction.id);
+        });
+
+
+        actionsCell.appendChild(editButton);
+        actionsCell.appendChild(deleteButton);
+
+        row.appendChild(dateCell);
+        row.appendChild(typeCell);
+        row.appendChild(categoryCell);
+        row.appendChild(descriptionCell);
+        row.appendChild(amountCell);
+        row.appendChild(actionsCell);
+
+        transactionList.appendChild(row);
     });
-
-
-    const amountDisplay=
-        transaction.type==="income"
-            ? `+ ₹${formattedAmount}`
-            : `- ₹${formattedAmount}`;
-
-    const dateCell=document.createElement("td");
-    dateCell.textContent=formattedDate;
-
-    const typeCell=document.createElement("td");
-
-    const typeSpan=document.createElement("span");
-    typeSpan.className=`transaction-type ${transaction.type}`;
-    typeSpan.textContent=transaction.type;
-
-    typeCell.appendChild(typeSpan);
-
-    const categoryCell=document.createElement("td");
-    categoryCell.textContent=transaction.category;
-
-
-    const descriptionCell=document.createElement("td");
-    descriptionCell.textContent=transaction.description || "-";
-
-    const amountCell=document.createElement("td");
-
-    amountCell.className=
-        transaction.type==="income"
-            ? "amount-income"
-            : "amount-expense";
-
-    amountCell.textContent=amountDisplay;
-
-    const actionsCell=document.createElement("td");
-
-
-    const editButton=document.createElement("button");
-    editButton.textContent="Edit";
-
-    editButton.addEventListener("click",function(){
-        editTransaction(transaction.id);
-    });
-
-
-    const deleteButton=document.createElement("button");
-    deleteButton.textContent="Delete";
-
-    deleteButton.addEventListener("click",function(){
-        deleteTransaction(transaction.id);
-    });
-
-
-    actionsCell.appendChild(editButton);
-    actionsCell.appendChild(deleteButton);
-    row.appendChild(dateCell);
-    row.appendChild(typeCell);
-    row.appendChild(categoryCell);
-    row.appendChild(descriptionCell);
-    row.appendChild(amountCell);
-    row.appendChild(actionsCell);
-
-    transactionList.appendChild(row);
-});
 }
 
+
+/*EDIT TRANSACTION*/
 
 function editTransaction(id){
 
@@ -328,14 +413,22 @@ function editTransaction(id){
 
 
     document.getElementById("type").value=transaction.type;
-    document.getElementById("amount").value=transaction.amount;
+
+    type.dispatchEvent(new Event("change"));
+
+
     document.getElementById("category").value=transaction.category;
+
+    document.getElementById("amount").value=transaction.amount;
     document.getElementById("date").value=transaction.date;
     document.getElementById("description").value=transaction.description;
+
 
     editingId=id;
 
     submitButton.textContent="Update Transaction";
+    cancelEdit.style.display="block";
+    formTitle.textContent="Edit Transaction";
 
 
     window.scrollTo({
@@ -344,6 +437,23 @@ function editTransaction(id){
     });
 }
 
+
+/*CANCEL EDIT*/
+
+cancelEdit.addEventListener("click",function(){
+
+    editingId=null;
+
+    transactionForm.reset();
+
+    submitButton.textContent="Add Transaction";
+    cancelEdit.style.display="none";
+    formTitle.textContent="Add a Transaction";
+    errorMessage.textContent="";
+});
+
+
+/*DELETE TRANSACTION*/
 
 function deleteTransaction(id){
 
@@ -371,6 +481,7 @@ confirmDelete.addEventListener("click",function(){
     updateMonthlySummary();
     updateExpenseChart();
 
+
     deletePopup.classList.remove("show");
 
     deleteId=null;
@@ -396,6 +507,8 @@ deletePopup.addEventListener("click",function(event){
 
 });
 
+
+/*OVERALL SUMMARY*/
 
 function updateSummary(){
 
@@ -435,25 +548,37 @@ function updateSummary(){
     })}`;
 }
 
+
+/*MONTHLY SUMMARY*/
+
 function updateMonthlySummary(){
+
     const selectedMonth=monthFilter.value;
     const selectedYear=yearFilter.value;
 
+
     if(selectedMonth===""||selectedYear===""){
+
         monthlyIncome.textContent="₹0.00";
         monthlyExpenses.textContent="₹0.00";
         monthlyBalance.textContent="₹0.00";
+
         return;
     }
+
 
     let income=0;
     let expenses=0;
 
+
     transactions.forEach(function(transaction){
+
         const transactionYear=transaction.date.substring(0,4);
         const transactionMonth=transaction.date.substring(5,7);
 
+
         if(transactionYear===selectedYear&&transactionMonth===selectedMonth){
+
             if(transaction.type==="income"){
                 income+=transaction.amount;
             }else{
@@ -462,17 +587,21 @@ function updateMonthlySummary(){
         }
     });
 
+
     const balance=income-expenses;
+
 
     monthlyIncome.textContent=`₹${income.toLocaleString("en-IN",{
         minimumFractionDigits:2,
         maximumFractionDigits:2
     })}`;
 
+
     monthlyExpenses.textContent=`₹${expenses.toLocaleString("en-IN",{
         minimumFractionDigits:2,
         maximumFractionDigits:2
     })}`;
+
 
     monthlyBalance.textContent=`₹${balance.toLocaleString("en-IN",{
         minimumFractionDigits:2,
@@ -480,6 +609,8 @@ function updateMonthlySummary(){
     })}`;
 }
 
+
+/*=EXPENSE CHART*/
 
 function updateExpenseChart(){
 
@@ -569,19 +700,76 @@ function updateExpenseChart(){
         expenseChart.appendChild(chartItem);
     });
 }
-const currentYear = new Date().getFullYear();
 
-for(let year=currentYear-5; year<=currentYear+5; year++){
+
+/* 
+   YEAR / MONTH FILTER SETUP
+*/
+
+const currentYear=new Date().getFullYear();
+
+for(let year=currentYear-5; year<=currentYear; year++){
+
     const option=document.createElement("option");
+
     option.value=String(year);
     option.textContent=year;
+
     yearFilter.appendChild(option);
 }
 
-const currentMonth = String(new Date().getMonth()+1).padStart(2,"0");
 
-monthFilter.value = currentMonth;
-yearFilter.value = String(currentYear);
+const currentMonthNumber=new Date().getMonth()+1;
+
+
+// for(let month=currentMonthNumber+1; month<=12; month++){
+
+//     const option=monthFilter.querySelector(
+//         `option[value="${String(month).padStart(2,"0")}"]`
+//     );
+
+//     if(option){
+//         option.remove();
+//     }
+// }
+
+function loadMonths(selectedYear){
+
+    monthFilter.innerHTML="";
+
+    let lastMonth=12;
+
+    if(Number(selectedYear)===currentYear){
+        lastMonth=currentMonthNumber;
+    }
+
+    for(let month=1; month<=lastMonth; month++){
+
+        const option=document.createElement("option");
+
+        const monthValue=String(month).padStart(2,"0");
+
+        option.value=monthValue;
+
+        option.textContent=new Date(2000,month-1).toLocaleString("en-IN",{
+            month:"long"
+        });
+
+        monthFilter.appendChild(option);
+    }
+}
+
+yearFilter.value=String(currentYear);
+
+loadMonths(String(currentYear));
+
+const currentMonth=String(currentMonthNumber).padStart(2,"0");
+
+monthFilter.value=currentMonth;
+yearFilter.value=String(currentYear);
+
+
+/*INITIAL DISPLAY*/
 
 displayTransactions();
 updateSummary();
